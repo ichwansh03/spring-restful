50e111271c477643703447ee6274d9cfceb88106
Ichwan <ichwansholihin03@gmail.com>
2023-11-12 20:17:30 +0700

    fix: move to directory response

--- src/main/java/com/ichwan/restful/controller/ErrorController.java
+++ src/main/java/com/ichwan/restful/controller/ErrorController.java
@@ -1,6 +1,6 @@
 package com.ichwan.restful.controller;
 
-import com.ichwan.restful.model.WebResponse;
+import com.ichwan.restful.model.response.WebResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
