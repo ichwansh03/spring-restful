3d5740d30c709bd7cba2e366ac1b315c4d686b2b
Ichwan <ichwansholihin03@gmail.com>
2023-11-12 20:18:10 +0700

    feat: add argument resolver for auth header token

--- src/main/java/com/ichwan/restful/repository/UserRepository.java
+++ src/main/java/com/ichwan/restful/repository/UserRepository.java
@@ -3,5 +3,9 @@ package com.ichwan.restful.repository;
 import com.ichwan.restful.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, String> {
+
+    Optional<User> findFirstByToken(String token);
 }
--- src/main/java/com/ichwan/restful/service/userservice/UserService.java
+++ src/main/java/com/ichwan/restful/service/userservice/UserService.java
@@ -1,12 +1,16 @@
 package com.ichwan.restful.service.userservice;
 
-import com.ichwan.restful.model.LoginUserRequest;
-import com.ichwan.restful.model.RegisterUserRequest;
-import com.ichwan.restful.model.TokenResponse;
+import com.ichwan.restful.entity.User;
+import com.ichwan.restful.model.request.LoginUserRequest;
+import com.ichwan.restful.model.request.RegisterUserRequest;
+import com.ichwan.restful.model.response.TokenResponse;
+import com.ichwan.restful.model.response.UserResponse;
 
 public interface UserService {
 
     void register(RegisterUserRequest request);
+
+    UserResponse get(User user);
 
     //untuk login user
     TokenResponse login(LoginUserRequest request);
